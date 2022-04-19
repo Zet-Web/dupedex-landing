f85c64c898ded4cc53d9ac29fb3118e0c66f5dee
Zair Akbarov <zet.pick@gmail.com>
2022-04-19 18:56:01 +0500

    One page landing

--- index.js
+++ index.js
@@ -1,94 +1,76 @@
 const authCheckUrl = 'https://rest.clicksend.com/v3/account'
 const username = 'raycai@hotmail.co.nz'
 const apiKey = '9D54BDB7-A845-6768-CE45-24D20616F883'
+const emailListId = 1451222;
 const basecode = btoa(`${username}:${apiKey}`)
 
 const modal = document.querySelector(".modal")
-const dialogVerify = document.querySelector(".dialog__verify_wrapper")
 const dialogMessage = document.querySelector(".dialog__message_wrapper")
-const messageBox = dialogMessage.querySelector(".dialog__caption")
+const messageCaption = dialogMessage.querySelector(".dialog__caption")
+const messageText = dialogMessage.querySelector(".dialog__text")
 
 const closeBtn = dialogMessage.querySelector(".close_modal")
 
-const phoneInput = document.querySelector('#phone_input')
-const errorMessage = document.querySelector('.hero-section__text_error')
+const emailInput = document.querySelector('#email_input')
+const errorMessage = document.querySelector('.main__text_error')
 
-const pinForm = document.querySelector('#pin_form');
-const pinInput = document.querySelector('#pin_input');
+// const dialogVerify = document.querySelector(".dialog__verify_wrapper")
+// const pinForm = document.querySelector('#pin_form');
+// const pinInput = document.querySelector('#pin_input');
 
-let mobilePin = 0;
+// let mobilePin = 0;
 
-function onSubmitPin() {
-    messageBox.textContent = (pinInput.value === mobilePin) ? "SUCCESS" : 'ERROR'
+// function onSubmitPin() {
+//     messageBox.textContent = (pinInput.value === mobilePin) ? "SUCCESS" : 'ERROR'
 
-    pinInput.value = ''
-    dialogVerify.classList.toggle("is-active")
-    dialogMessage.classList.toggle("is-active")
-    closeBtn.focus()
+//     pinInput.value = ''
+//     dialogVerify.classList.toggle("is-active")
+//     dialogMessage.classList.toggle("is-active")
+//     closeBtn.focus()
+// }
+
+// function validatePin(e) {
+//     const theEvent = e || window.event;
+//     let key = theEvent.keyCode || theEvent.which;
+//     if (key !== 13) {
+//         key = String.fromCharCode( key );
+//         const regex = /[0-9]|\./;
+//         if( !regex.test(key) || theEvent.target.value.length >= 4) {
+//         theEvent.returnValue = false;
+//         if (theEvent.preventDefault) theEvent.preventDefault();
+//         }
+//     }
+// }
+
+// pinForm.addEventListener('submit', e => {
+//     e.preventDefault();
+//     onSubmitPin();
+// })
+// pinInput.addEventListener('keypress', e => validatePin(e))
+    // if (isEmailValid) {
+    //     console.log(`sms fake-sent to email: ${emailInput.value}`)
+
+    //     mobilePin = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1)
+    //     console.log(mobilePin)
+    //     alert(mobilePin)
+
+    //     modal.classList.toggle("is-active")
+    //     dialogVerify.classList.toggle("is-active")
+    //     document.body.classList.toggle("not-scrollable")
+
+    //     pinInput.focus()
+    //     errorMessage.textContent = ''
+    // }
+
+const validateEmail = (email) => {
+    const emailRegExp = /[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/i;
+    return email.match(emailRegExp)
 }
 
-function validatePin(e) {
-    const theEvent = e || window.event;
-    let key = theEvent.keyCode || theEvent.which;
-    if (key !== 13) {
-        key = String.fromCharCode( key );
-        const regex = /[0-9]|\./;
-        if( !regex.test(key) || theEvent.target.value.length >= 4) {
-        theEvent.returnValue = false;
-        if (theEvent.preventDefault) theEvent.preventDefault();
-        }
-    }
-}
-
-function validatePhone(e) {
-    const theEvent = e || window.event;
-    let key = theEvent.keyCode || theEvent.which;
-    if (key !== 13) {
-        key = String.fromCharCode( key );
-        const regex = /[0-9]|\+/;
-        if( !regex.test(key)) {
-        theEvent.returnValue = false;
-        if (theEvent.preventDefault) theEvent.preventDefault();
-        }
-    }
-}
-
-const fakeVerifying = (phone) => {
-
-    const phoneRegexp = /^\+{1}([0-9]{4,13})$/g
-    const isPhoneValid = phone.match(phoneRegexp)
-
-    if (isPhoneValid) {
-        mobilePin = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1)
-        console.log(mobilePin)
-        alert(mobilePin)
-
-        modal.classList.toggle("is-active")
-        dialogVerify.classList.toggle("is-active")
-        document.body.classList.toggle("not-scrollable")
-
-        pinInput.focus()
-        errorMessage.textContent = ''
-    }
-    else errorMessage.textContent = 'Please enter valid number : +64.....'
-}
-
-
-pinForm.addEventListener('submit', e => {
-    e.preventDefault();
-    onSubmitPin();
-})
-
-phoneInput.addEventListener('keypress', e => validatePhone(e))
-pinInput.addEventListener('keypress', e => validatePin(e))
-
-document.querySelectorAll('.close_modal').forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        modal.classList.remove("is-active")
-        dialogVerify.classList.remove("is-active")
-        dialogMessage.classList.remove("is-active")
-        document.body.classList.remove("not-scrollable")
-    })
+closeBtn.addEventListener('click', (e) => {
+    modal.classList.remove("is-active")
+    dialogMessage.classList.remove("is-active")
+    document.body.classList.remove("not-scrollable")
 })
 
 const checkAuth = () => {
@@ -112,33 +94,35 @@ checkAuth();
 document.querySelector('#sent_button').addEventListener('click', (e) => {
     e.preventDefault();
 
-    console.log(`sms fake-sent to phone number: ${phoneInput.value}`)
-    fakeVerifying(phoneInput.value)
+    const EmailUrl = `https://rest.clicksend.com/v3/lists/${emailListId}/contacts`
+    const sendOptions = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Basic ${basecode}`,
+        },
+        body: JSON.stringify({
+            "last_name": "Client",
+            "first_name": "Test",
+            "email": emailInput.value
+        })
+    }
 
-    // DONT UNCOMMENT!! OR PAY FOR SMS SENDING!!
 
-    // const sendSMSUrl = 'https://rest.clicksend.com/v3/sms/send'
-    // const sendOptions = {
-    //     method: "POST",
-    //     mode: "cors",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         "Accept": "application/json",
-    //         "Authorization": `Basic ${basecode}`,
-    //     },
-    //     body: JSON.stringify({
-    //         "messages": [
-    //           {
-    //             "to": `${phoneInput}`,
-    //             "source": "Dupe.dex",
-    //             "body": "Hello. Please verify your phone number with PIN: ####"
-    //           }
-    //         ]
-    //       })
-    // }
-    // fetch(sendSMSUrl, sendOptions)
-    //     .then(response => response.json())
-    //     .then((result) => {
-    //         return console.log(result)})
+    if (validateEmail(emailInput.value)) {
+        errorMessage.textContent = '';
+        fetch(EmailUrl, sendOptions)
+        .then(response => response.json())
+        .then((result) => {
+            messageCaption.textContent = (result.http_code === 200) ? "You are subscribed now!" : 'Something goes wrong...';
+            messageText.textContent = (result.http_code === 200) ? "We will keep you in touch when app is ready" : 'Please try again';
+            document.body.classList.add("not-scrollable")
+            modal.classList.add("is-active")
+            dialogMessage.classList.add("is-active")
+            closeBtn.focus()
+        })
+    } else errorMessage.textContent = 'Please enter valid E-mail like: mymail@gmail.com'
 })
 
